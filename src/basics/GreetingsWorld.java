3fe8f9d452989f0b5c562d4415b1748ec2763bdf
Muhammad <cs21.bilal@gmail.com>
2022-09-22 17:39:09 +0500

    Modified code

---
+++ src/basics/GreetingsWorld.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class GreetingsWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Java Program!");
+		System.out.println("Adding new code to my project.");
+	}
+
+}
